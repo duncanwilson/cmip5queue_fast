b4ff8ff8b0974ea54be6c381862aa42ed543915a
Duncan Wilson <dunc.wilson9@gmail.com>
2017-11-01 15:29:40 -0500

    update fast queue

--- cmip5queue_fast/tasks/tasks.py
+++ cmip5queue_fast/tasks/tasks.py
@@ -21,29 +21,16 @@ def check_task_storage(args):
     resultDir = os.path.join(basedir, 'cmip5_tasks/', user_id, task_id, 'output/')
     task_space = get_size(start_path = resultDir)
     return task_space
-    
-@task()
-def check_user_storage(args):
-    """
-        Checks user storage space from previous tasks
-        args: user_id
-        example {"user_id":"duncan"}
-        return size in bytes
-    """
-    user_id = args['user_id']
-    resultDir = os.path.join(basedir, 'cmip5_tasks/', user_id)
-    user_space = get_size(start_path = resultDir)
-    return user_space    
 
 @task()
 def delete_task_data(args):    
     """
         Deletes user storage space from a specific task
-        args: {user_id, History_task_id}
-        note: history_task_id is from a previously completed task
+        args: {user_id, history_task_id}
+        note: history_task_id is from a previously completed task. This is the celery id.
         returns space deleted in bytes
-        example: {"user_id":"duncan", "task_id":"trialrun"}
-        function deletes entire subfolder "/output" from a task folder
+        example: {"user_id":"duncan", "task_id":"c57b560e-e46c-4eca-85e7-e856e871a2e8"}
+        function deletes entire subfolder "/output" from the specified task folder
     """
     task_id = args['history_task_id']
     user_id = args['user_id']
@@ -73,24 +60,6 @@ def create_tables(args):
     return
     
 @task()
-def create_tables_old(args):
-    """
-        Runs R script to create html tables of the CMIP5 data selected
-        Uses Docker_task
-        args are:
-              {user_id,
-              final_query}
-    """
-    task_id = str(create_tables.request.id)
-    user_id = args['user_id']
-    resultDir = setup_user_directory(user_id)
-    with open(resultDir + '/tables.json', "wt") as f:
-        jsonx.dump(args,f)
-    docker_opts = "--rm -v /data/cmip5_functions:/sccsc:ro -v /data/static_web/cmip5_tasks:/sccsc_out -w /sccsc"
-    docker_cmd ="Rscript /sccsc/cmip5_tables.R {0}".format(user_id)
-    result = docker_task(docker_name="sccsc/r",docker_opts=docker_opts,docker_command=docker_cmd,id=task_id)
-    return result
-    
 def get_size(start_path = '.'):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
@@ -103,9 +72,11 @@ def setup_user_directory(user_id):
     resultDir = os.path.join(basedir, 'cmip5_tasks/', user_id)
     try:
         os.makedirs(resultDir)
+        os.makedirs("{0}/tables".format(resultDir))
     except OSError as err:
         if err.errno!=17:
             raise
     os.chmod(resultDir,0777)
+    os.chmod("{0}/tables".format(resultDir),0777)
     return resultDir
 
